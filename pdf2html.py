1a170cf8c541e07dda446313102233d6e3e5655d
fecet <xiezej@gmail.com>
2022-03-30 18:30:24 +0800

    feat add open-files option

--- pdf2html.py
+++ pdf2html.py
@@ -31,32 +31,39 @@ parser.add_argument("pdf_path", help="The PDF document file path to be converted
 parser.add_argument(
     "html_path", nargs="?", default="", help="The HTML document file path to be stored"
 )
+parser.add_argument(
+    "--open_files", "-o", help="Open html and PDF files", action="store_true"
+)
 args = parser.parse_args()
 
 
 # %%
 
 
-pdf_path = Path(args.pdf_path)  # pdf_path = Path("test.pdf")
-html_path = (
-    Path(args.html_path)
-    if args.html_path
-    else pdf_path.parent / f"{pdf_path.stem}.html"
-)
-if not html_path.exists():
-    subprocess.run(["pdf2htmlEX", pdf_path, html_path])
-    # Post process for the HTML file
-    # Delete TOC
-    with open(html_path, "r+") as f:
-        soup = BeautifulSoup(f, "html.parser")
-        elements = soup.find_all("div", id="sidebar")
-        for element in elements:
-            element.decompose()
+def pdf2html(pdf_path, html_path="", open_files=False):
 
-        f.write(str(soup))
+    pdf_path = Path(pdf_path)  # pdf_path = Path("test.pdf")
+    html_path = (
+        Path(html_path) if html_path else pdf_path.parent / f"{pdf_path.stem}.html"
+    )
+    if not html_path.exists():
+        subprocess.run(["pdf2htmlEX", pdf_path, html_path])
+        # Post process for the HTML file
+        # Delete TOC
+        with open(html_path, "r+") as f:
+            soup = BeautifulSoup(f, "html.parser")
+            elements = soup.find_all("div", id="sidebar")
+            for element in elements:
+                element.decompose()
+
+            f.write(str(soup))
+    if open_files:
+
+        subprocess.Popen(["xdg-open", pdf_path], start_new_session=True)
+        subprocess.Popen(["xdg-open", html_path], start_new_session=True)
 
 
 # %%
 
-subprocess.Popen(["xdg-open", pdf_path], start_new_session=True)
-subprocess.Popen(["xdg-open", html_path], start_new_session=True)
+if __name__ == "__main__":
+    pdf2html(args.pdf_path, args.html_path, args.open_files)
